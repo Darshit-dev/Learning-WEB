c1763601ed896be1faf614babbc445d5f05fc029
Darshit-dev <khenidarshit@gmail.com>
2022-06-25 19:54:45 +0530

    till 25

--- YT DSA/compareTwoObj.js
+++ YT DSA/compareTwoObj.js
@@ -1,29 +1,30 @@
-function anagram(stringA,stringB){
-    try{
-    stringA=stringA.toLowerCase();
-     stringB=stringB.toLowerCase();
-    if(stringA.length!==stringB.length){return false}
-    const stringACharCount={}
-    for(i=0;i<stringA.length;i++){
-      let charA=stringA[i]
-    stringACharCount[charA] =stringACharCount[charA]+1||1
-      
+function anagram(stringA, stringB) {
+  try {
+    stringA = stringA.toLowerCase();
+    stringB = stringB.toLowerCase();
+    if (stringA.length !== stringB.length) {
+      return false;
     }
-      let stringBCharCount={}
-   for(i=0;i<stringB.length;i++){
-     let charB=stringB[i]
-      stringBCharCount[charB] =stringBCharCount[charB]+1||1
-     
-   }
-        console.log(JSON.stringify(stringBCharCount))
-        console.log(JSON.stringify(stringACharCount))
-     if(JSON.stringify(stringACharCount)===JSON.stringify(stringBCharCount)){
-       return true}
-      else{
-        return false 
-       }
+    const stringACharCount = {};
+    for (i = 0; i < stringA.length; i++) {
+      let charA = stringA[i];
+      stringACharCount[charA] = stringACharCount[charA] + 1 || 1;
     }
-    catch(err){
-      return(err.message)}}
-  
-  console.log(anagram("AHbi","abhi"))
+    let stringBCharCount = {};
+    for (i = 0; i < stringB.length; i++) {
+      let charB = stringB[i];
+      stringBCharCount[charB] = stringBCharCount[charB] + 1 || 1;
+    }
+    console.log(JSON.stringify(stringBCharCount));
+    console.log(JSON.stringify(stringACharCount));
+    if (JSON.stringify(stringACharCount) === JSON.stringify(stringBCharCount)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    return err.message;
+  }
+}
+
+console.log(anagram("AHbi", "abhi"));
